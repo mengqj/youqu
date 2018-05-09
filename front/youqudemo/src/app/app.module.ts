0f2881334353657cb1b89b07885eb6859229038c
gaojing <646040659@qq.com>
2018-05-09 11:15:07 +0800

    添加（主页、发起同游页、加号页）所需服务、模块

--- front/youqudemo/src/app/app.module.ts
+++ front/youqudemo/src/app/app.module.ts
@@ -2,14 +2,18 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { MultiPickerModule } from 'ion-multi-picker';
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { LaunchPage } from '../pages/launch/launch';
+import { PlusPage } from '../pages/plus/plus';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { CityDataProvider } from '../providers/city-data/city-data';
 
 @NgModule({
   declarations: [
@@ -17,11 +21,17 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    LaunchPage,
+    PlusPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp,{
+      tabsHideOnSubPages: true,
+      backButtonIcon: 'ios-arrow-back'
+    }),
+    MultiPickerModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -29,12 +39,15 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    LaunchPage,
+    PlusPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    CityDataProvider
   ]
 })
 export class AppModule {}
