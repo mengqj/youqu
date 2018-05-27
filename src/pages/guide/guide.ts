388c0b5a2740de5ec38686c440f8d0515257d493
gaojing <646040659@qq.com>
2018-05-27 18:24:34 +0800

    修改攻略页

--- src/pages/guide/guide.ts
+++ src/pages/guide/guide.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component,OnInit,ElementRef} from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ContentPage } from '../content/content';
+import * as $ from 'jquery';
 
 /**
  * Generated class for the GuidePage page.
@@ -18,15 +19,32 @@ export class GuidePage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
+
+  items=[{
+    title:'北京',
+    img:'assets/imgs/item1.png'
+  },
+  {
+    title:'厦门',
+    img:'assets/imgs/item2.png'
+  },
+  {
+    title:'上海',
+    img:'assets/imgs/item3.png'
+  }];
   
 show="show1";
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad GuidePage');
-  }
+ionViewDidLoad(){
+  $(".col1").click(function(){
+    var title = $(this).find('.title').text();
+    console.log(title);
+  });
+}
 
-  content(){
-    this.navCtrl.push(ContentPage);
+  place;
+  goDetail(){
+    //this.place=this.title;
+    this.navCtrl.push(ContentPage,{place:this.place});
+    console.log(this.place);
   }
-  
-  
 }
