39f59a479b7aadbef8a0e0cfff18614f2d1f5a85
xuejiawei1 <anotheryx@outlook.com>
2018-05-29 08:09:05 +0800

    fix

--- front/new pages/sendtext/sendtext.ts
+++ front/new pages/sendtext/sendtext.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams,ViewController } from 'ionic-angula
 import {HttpClient} from "@angular/common/http";
 import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer';
 import { File } from '@ionic-native/file';
+import  * as $ from 'jquery';
 @IonicPage()
 @Component({
   selector: 'page-sendtext',
@@ -15,9 +16,56 @@ export class SendtextPage {
   {
 
   }
+  ionViewDidLoad(){
+    $('#btn').on('click',function(){
+      console.log($('#page').html());
+
+    })
+  }
   words='';
-  path: string="../../assets/imgs/zijingcheng.jpeg";
   fileTransfer: FileTransferObject = this.transfer.create();
+val;
+src;
+upload(){
+  //let file = (<HTMLInputElement>$('#img')).files[0];
+  //let file = document.getElementById("choose").files[0];
+  let file=(<HTMLInputElement>document.getElementById('img')).files[0];
+  console.log(file);
+            let formData = new FormData();
+            formData.append('file',file);
+            $.ajax({
+                type: 'POST',
+                url: 'http://localhost:8080/system/upload?type=images',
+                data: formData,
+                async: false,
+                cache: false,
+                contentType: false,
+                processData: false,
+                success: function (data) {
+                    console.log(data);
+
+                    $("#show").attr("src", 'http://localhost:8080'+data);
+                },
+                error: function (err) {
+                    console.log(err);
+                }
+            })
+}
+/*upload(){
+  console.log(this.val);
+  this.src=this.val;
+  console.log(this.src);
+
+  let url:string="http://localhost:8080/system/upload?type=image";
+  var formData = new FormData();
+  let file='';
+  formData.append('file',file);
+  this.http.post(url,formData).subscribe(
+    data => {
+        console.log(data);
+        }
+  );
+}*/
 
 send(){
   console.log(this.words);
