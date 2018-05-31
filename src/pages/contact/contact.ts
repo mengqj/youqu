928eb7d1c35959a000db01f2ab8f21d8490913ae
xuejiawei1 <anotheryx@outlook.com>
2018-05-31 14:45:54 +0800

    修改 同游展示页面(联系后台)

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import {HttpClient} from "@angular/common/http";
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
@@ -8,30 +8,33 @@ import { NavController } from 'ionic-angular';
 export class ContactPage {
   searchQuery: string = '';
   items: string[];
-  constructor(public navCtrl: NavController) {
-    this.initializeItems();
+  constructor(public navCtrl: NavController,private http: HttpClient) {
   }
-  initializeItems() {
-    this.items = [
-      '上海',
-      '北京',
-      '北京',
-      '北京',
-      '北京',
-      '北京',
-      '深圳',
-      '福建',
-      '湖南',
-      '广州',
-      '广州',
-      '广州',
-      '广州',
-      '广州',
-      '福建'
-    ];
+  val=new Array();
+  ionViewDidLoad(){
+  let host='35.194.153.183';
+  let url:string='http://'+host+':8080/api/content/getSimpleListByParams?typeId=H13H1FjJQ';
+  this.http.get(url)
+  .subscribe(
+  data =>{
+    this.val = data['docs'];
+    console.log(data);
+
+    for(var i=0;i<this.val.length;i++){
+      this.val[i].des=data['docs'][i].title;
+      this.val[i].loc=data['docs'][i].stitle;
+      this.val[i].name=data['docs'][i].uAuthor.userName;
+      this.val[i].godate=data['docs'][i].discription;
+      this.val[i].content=data['docs'][i].markDownComments;
+      this.val[i].updateDate=data['docs'][i].updateDate;
+      this.val[i].logo='http://'+host+':8080'+data['docs'][i].uAuthor.logo;
+    }
+  });
   }
 
-  getItems(ev: any) {
+
+
+  /*getItems(ev: any) {
     this.initializeItems();
     let val = ev.target.value;
     if (val && val.trim() != '') {
@@ -39,6 +42,6 @@ export class ContactPage {
         return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
       })
     }
-  }
+  }*/
 }
 
