0cfffc1ca970f070fe2be2e5cd3e2ce9f85cbb73
xuejiawei1 <anotheryx@outlook.com>
2018-06-05 14:12:54 +0800

    添加 推荐 发布消息 提问 问答

--- src/pages/question/question.ts
+++ src/pages/question/question.ts
@@ -1,25 +1,73 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the QuestionPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
+import { IonicPage, NavController,NavParams,ModalController} from 'ionic-angular';
+import {HttpClient} from "@angular/common/http";
 @IonicPage()
 @Component({
   selector: 'page-question',
   templateUrl: 'question.html',
 })
 export class QuestionPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public modalCtrl: ModalController,private http: HttpClient) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad QuestionPage');
+  ionViewWillEnter() {
+    this.getQuestion();
+    this.getUserQuestion();
+  }
+  val=new Array();
+  valt=new Array();
+  host='35.194.153.183';
+  user="BJpVFFMRG";
+  typeId='Bkxe_nDRG';
+  //获取推荐问题
+  getQuestion(){
+    let url:string='http://'+this.host+':8080/api/content/getList?typeId='+this.typeId;
+    this.http.get(url)
+    .subscribe(
+    (data:any) =>{
+      this.val = data['docs'];
+      console.log(data);
+     for(var i=0;i<this.val.length;i++){
+      this.val[i].logo='http://'+this.host+':8080'+data.docs[i].uAuthor.logo;
+      this.val[i].name=data.docs[i].uAuthor.userName;
+      this.val[i].title=data.docs[i].title;
+      this.val[i].commentNum=data.docs[i].commentNum;
+      this.val[i].date=data.docs[i].date;
+      this.val[i].content=data.docs[i].markDownComments;
+      this.val[i].id=data.docs[i].id;
+      //console.log(data.docs[i].uAuthor.logo);
+      }
+    });
+  }
+  //获取用户提问
+  getUserQuestion(){
+    let url:string='http://'+this.host+':8080/api/content/getList?typeId='+this.typeId+'&user='+this.user;
+    this.http.get(url)
+    .subscribe(
+    (data:any) =>{
+      this.valt= data['docs'];
+      console.log(data);
+      for(var i=0;i<this.valt.length;i++){
+      this.valt[i].logo='http://'+this.host+':8080'+data.docs[i].uAuthor.logo;
+      this.valt[i].name=data.docs[i].uAuthor.userName;
+      this.valt[i].title=data.docs[i].title;
+      this.valt[i].commentNum=data.docs[i].commentNum;
+      this.valt[i].date=data.docs[i].date;
+      this.valt[i].content=data.docs[i].markDownComments;
+      this.valt[i].id=data.docs[i].id;
+      }
+    });
+  }
+  //获取用户回答目前无法实现
+  getUserRequest(){
+
   }
   question:string="recommend";
+  ask(){
+    let modal = this.modalCtrl.create('AskPage');
+      modal.present();
+  }
+  show(id){
+    this.navCtrl.push('ClicktextPage',{textId:id});
+  }
 }
