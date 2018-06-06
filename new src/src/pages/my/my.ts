3202a7a5d40f7ccdae34c3498a2632e4d22bdae0
mengqj <le371645070@gmail.com>
2018-06-06 11:11:18 +0800

    Signed-off-by: mengqj <le371645070@gmail.com>

--- new src/src/pages/my/my.ts
+++ new src/src/pages/my/my.ts
@@ -21,21 +21,21 @@ export class MyPage {
 
   
   ionViewDidLoad(){
-this.user=this.storage.getItem(this.username);
+this.user=this.storage.getItem('ID');
 console.log(this.user);
     let host='35.194.153.183';
     let host2='localhost';
     let url:string='http://35.194.153.183:8080/api/users/getUserContents?user='+this.user;
-    
+    let url1:string=':http://35.194.153.183:8080/api/users/getUser?searchkey='+this.user;
     
     this.http.get(url)
     .subscribe(
     data =>{
       this.val = data['docs'];
       for(var i=0;i<this.val.length;i++){
-        this.val[i].title=data['docs'][i].title;
+        this.val[i].title=this.storage.getItem('userName');
         this.val[i].updateDate=data['docs'][i].updateDate.substr(5,5);
-        this.val[i].sImg='http://'+host+':8080'+data['docs'].sImg;
+        // this.val[i].sImg='http://'+host+':8080'+data['docs'].sImg;
         this.val[i].content=data['docs'][i].markDownComments ;
         console.log(this.val[i].updateDate);
 
@@ -46,23 +46,22 @@ console.log(this.user);
 
     });
 
-
+  
   };
 show="information"
 
 item=[{
-  focus:100,
-  fans:20,
-  favor:100,
+  name:this.storage.getItem('userName')
+ 
 }];
 setting(){
-  this.navCtrl.push(SettingPage);
+  this.navCtrl.push('SettingPage');
 }
 
 doionrefresh(ionrefresher){
 
   setTimeout(()=>{
-    this.user=this.storage.getItem(this.username);
+    this.user=this.storage.getItem('ID');
 console.log(this.user);
     let host='35.194.153.183';
     let host2='localhost';
@@ -74,9 +73,9 @@ console.log(this.user);
     data =>{
       this.val = data['docs'];
       for(var i=0;i<this.val.length;i++){
-        this.val[i].title=data['docs'][i].title;
+        this.val[i].title=this.storage.getItem('userName');
         this.val[i].updateDate=data['docs'][i].updateDate.substr(5,5);
-        this.val[i].sImg='http://'+host+':8080'+data['docs'].sImg;
+        // this.val[i].sImg='http://'+host+':8080'+['docs'].sImg;
         this.val[i].content=data['docs'][i].markDownComments ;
         console.log(this.val[i].updateDate);
 
