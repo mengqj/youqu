89bec83bec52efebff4c8ceee6eaa2c345b6e2ac
xuejiawei1 <anotheryx@outlook.com>
2018-06-06 15:48:17 +0800

    修改 主页

--- new src/src/pages/home/home.ts
+++ new src/src/pages/home/home.ts
@@ -3,15 +3,12 @@ import { NavController,ModalController } from 'ionic-angular';
 import { ViewChild } from '@angular/core';
 import { Slides } from 'ionic-angular';
 import { Platform, ToastController, App, Tabs } from 'ionic-angular';
-import { ContentPage } from '../content/content';
-import { NewslistPage } from '../newslist/newslist';
-import { ThumbsupPage } from '../thumbsup/thumbsup';
 import { HttpClient} from "@angular/common/http";
-import { ClicktextPage } from '../clicktext/clicktext';
-    @Component({
+
+@Component({
       selector: 'page-home',
       templateUrl: 'home.html'
-    })
+})
     export class HomePage {
       @ViewChild(Slides) slides: Slides;
       items = [];
@@ -21,8 +18,8 @@ import { ClicktextPage } from '../clicktext/clicktext';
           this.platform.ready().then(() => {
             this.registerBackButtonAction(null);
           });
+          this.getLocation();
       }
-
       ionViewWillEnter() {
         this.getlun();
         this.getAd();
@@ -34,6 +31,16 @@ import { ClicktextPage } from '../clicktext/clicktext';
       len;
       host='35.194.153.183';
       typeId='HJMQ4wCJQ';
+      location;
+      //获取定位
+      getLocation(){
+        let url:string='http://api.map.baidu.com/location/ip?ak=yvuRyfeZeWrT6KxGgGK5G5MU1bI4L8tf&coor=bd09ll&callback=jsonpCallback';
+    this.http.jsonp(url,'callback').subscribe(
+      (data:any)=>{
+        let city:any=data.content.address_detail.city;
+        this.location=city;
+      });
+      }
     //获取广告
       getAd(){
       let url:string='http://'+this.host+':8080/api/content/getList?typeId='+this.typeId;
@@ -41,7 +48,7 @@ import { ClicktextPage } from '../clicktext/clicktext';
       .subscribe(
       (data:any) =>{
         this.ad = data['docs'];
-        console.log(data);
+        //console.log(data);
        for(var i=0;i<this.ad.length;i++){
         this.ad[i].sImg='http://'+this.host+':8080'+data.docs[i].sImg;
         }
@@ -55,10 +62,10 @@ import { ClicktextPage } from '../clicktext/clicktext';
       .subscribe(
       (data:any) =>{
         this.val = data['docs'];
-        console.log(data);
+        //console.log(data);
        for(var i=0;i<this.val.length;i++){
         this.val[i].sImg='http://'+this.host+':8080'+data.docs[i].sImg;
-    
+
         }
       });
       }
@@ -70,19 +77,19 @@ import { ClicktextPage } from '../clicktext/clicktext';
       .subscribe(
       (data:any) =>{
         this.lunbo = data['docs'];
-        console.log(data);
+        //console.log(data);
         this.len=this.lunbo.length;
        for(var i=0;i<this.lunbo.length;i++){
         this.lunbo[i].sImg='http://'+this.host+':8080'+data.docs[i].sImg;
         }
       });
       }
-  
+
        //控制硬件返回按钮是否触发，默认false
        backButtonPressed: boolean = false;
       //注册方法
       registerBackButtonAction(tabRef: Tabs): void {
-    
+
         //registerBackButtonAction是系统自带的方法
         this.platform.registerBackButtonAction(() => {
           //获取NavController
@@ -119,49 +126,18 @@ import { ClicktextPage } from '../clicktext/clicktext';
           setTimeout(() => this.backButtonPressed = false, 2000);
         }
       }
-
+    //搜索
+    search(){
+      this.navCtrl.push('SearchPage');
+    }
     //跳转
     content(id){
-      this.navCtrl.push(ClicktextPage,{textId:id});
+      this.navCtrl.push('ClicktextPage',{textId:id});
     }
-    
+
     openlist(){
-      this.navCtrl.push(NewslistPage);
+      this.navCtrl.push('NewslistPage');
     }
   }
-  
 
-  //下拉加载
-  //doInfinite(infiniteScroll) {
-  //  console.log('Begin async operation');
-
-  //  setTimeout(() => {
-  //        title:'香港 | 拿上这份徒步攻略',
-  //        img:'assets/imgs/index-thumbnail2.png',
-  //        title2:'不要以为这又是一次shopping之旅',
-  //        count:'22727次浏览',
-  //        btn:'攻略'
-  //      },
-  //      {
-  //        title:'有哪些地方可以拍出不一样的重庆？',
-  //        img:'assets/imgs/index-thumbnail1.png',
-  //        title2:'史上最全的27个机位，让你看到一个更全面的重庆',
-  //        count:'22727次浏览',
-  //        btn:'问答'
-  //      },
-  //      {
-  //        title:'有哪些地方可以拍出不一样的重庆？',
-  //        img:'assets/imgs/index-thumbnail1.png',
-  //        title2:'史上最全的27个机位，让你看到一个更全面的重庆',
-  //        count:'22727次浏览',
-  //        btn:'问答'
-  //      });
-
-  //    infiniteScroll.complete();
-  //    if(this.items.length==9){
-  //      console.log(this.items.length);
-  //      infiniteScroll.enable(false);
-  //    }
-  //  }, 500);
-  //}
 
