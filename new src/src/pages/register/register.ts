dbba1fbbf16c335a9b1a7b8b42d267df4fb498a4
xuejiawei1 <anotheryx@outlook.com>
2018-06-08 10:17:35 +0800

    修改 登录 注册

--- new src/src/pages/register/register.ts
+++ new src/src/pages/register/register.ts
@@ -1,16 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { LoginPage }  from '../login/login';
 import { HttpClient } from '@angular/common/http';
 import { AlertController } from 'ionic-angular';
-
-/**
- * Generated class for the RegisterPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
+import  * as $ from 'jquery';
 @IonicPage()
 @Component({
   selector: 'page-register',
@@ -20,7 +12,6 @@ export class RegisterPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams,public http:HttpClient,public alertCtrl:AlertController) {
   }
-
   username:string;
   password:string;
   password1:string;
@@ -31,23 +22,26 @@ export class RegisterPage {
   changePwd(e){
     this.password=e.target.value;
     }
-    
+
    changeUnm(e){
      this.username=e.target.value;
      }
+
   goAbo(){
-    let url:string='http://35.194.153.183:8080/api/users/doReg';
-this.http.post(url,{
+    $('#reg,#log').attr("disabled","true");
+  let url:string='http://35.194.153.183:8080/api/users/doReg';
+  this.http.post(url,{
   'userName': this.username,
   'password': this.password,
   'confirmPassword':this.password1,
 }).subscribe(data =>{
   if(data['state']=="success")
   {
-    this.navCtrl.push(LoginPage);
+    this.navCtrl.push('LoginPage');
   }
   else if(data['state']=="error")
   {
+    $('#reg,#log').removeAttr("disabled");
     let alert = this.alertCtrl.create({
       title: "注册失败",
       subTitle: '密码位数不小于6位',
@@ -57,6 +51,7 @@ this.http.post(url,{
   }
   else if(data['state']=="error"&&this.username.length<5)
   {
+    $('#reg,#log').removeAttr("disabled");
     let alert = this.alertCtrl.create({
       title: "用户名错误",
       subTitle: '有户名应为'+data['message'],
@@ -66,6 +61,7 @@ this.http.post(url,{
   }
   else if(data['state']=="error"&&this.password!=this.password1)
   {
+    $('#reg').removeAttr("disabled");
     let alert = this.alertCtrl.create({
       title: "密码错误",
       subTitle: data['message'],
@@ -73,9 +69,7 @@ this.http.post(url,{
     });
     alert.present();
   }
-  
+
 })
-   }
-  
- 
+}
 }
